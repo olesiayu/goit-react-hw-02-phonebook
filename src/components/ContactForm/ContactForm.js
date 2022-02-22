6f48d3fea2f4cb07ba61f473caf3911bfe7b56ea
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2022-02-22 21:21:48 +0200

    allert

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -35,33 +35,32 @@ export class ContactForm extends Component {
 <form onSubmit={this.handleSubmit}>
   <label htmlFor={this.nameInputId}>Name
      <input
-                    type="text"
-                    name="name"
-            value={this.state.name}
-                    onChange={this.handleChange}
-                    id={this.nameInputId}
+        type="text"
+        name="name"
+        value={this.state.name}
+        onChange={this.handleChange}
+        id={this.nameInputId}
   
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   required
 />
-            </label>
-            <label htmlFor={this.numberInputId}>Number
-                <input
-  type="tel"
-                    name="number"
-                    value={this.state.number}
-                    onChange={this.handleChange}
-                    id={this.numberInputId}
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  </label>
+  <label htmlFor={this.numberInputId}>Number
+      <input
+        type="tel"
+        name="number"
+        value={this.state.number}
+        onChange={this.handleChange}
+        id={this.numberInputId}
+            
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
 />
-            </label>
-            <button type="submit">Add contact</button>
-</form>
-
-      
+  </label>
+      <button type="submit">Add contact</button>
+</form>      
     )
   }
 };
